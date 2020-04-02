1a530800b034c1170a5abd01281dc72edd2b0b8e
Diogo <diogomqbm13@gmail.com>
2020-04-02 12:07:29 -0300

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import LinkListPage from './LinkListPage'
+
+class App extends Component {
+  render() {
+    return (
+      <LinkListPage />
+    )
+  }
+}
+
+export default App
---
+++ src/components/__generated__/LinkList_viewer.graphql.js
@@ -0,0 +1,134 @@
+/**
+ * @flow
+ */
+
+/* eslint-disable */
+
+'use strict';
+
+/*::
+import type { ReaderFragment } from 'relay-runtime';
+type Link_link$ref = any;
+import type { FragmentReference } from "relay-runtime";
+declare export opaque type LinkList_viewer$ref: FragmentReference;
+declare export opaque type LinkList_viewer$fragmentType: LinkList_viewer$ref;
+export type LinkList_viewer = {|
+  +allLinks: ?{|
+    +edges: ?$ReadOnlyArray<?{|
+      +node: ?{|
+        +$fragmentRefs: Link_link$ref
+      |}
+    |}>
+  |},
+  +$refType: LinkList_viewer$ref,
+|};
+export type LinkList_viewer$data = LinkList_viewer;
+export type LinkList_viewer$key = {
+  +$data?: LinkList_viewer$data,
+  +$fragmentRefs: LinkList_viewer$ref,
+  ...
+};
+*/
+
+
+const node/*: ReaderFragment*/ = {
+  "kind": "Fragment",
+  "name": "LinkList_viewer",
+  "type": "Viewer",
+  "metadata": {
+    "connection": [
+      {
+        "count": null,
+        "cursor": null,
+        "direction": "backward",
+        "path": [
+          "allLinks"
+        ]
+      }
+    ]
+  },
+  "argumentDefinitions": [],
+  "selections": [
+    {
+      "kind": "LinkedField",
+      "alias": "allLinks",
+      "name": "__LinkList_allLinks_connection",
+      "storageKey": null,
+      "args": null,
+      "concreteType": "LinkConnection",
+      "plural": false,
+      "selections": [
+        {
+          "kind": "LinkedField",
+          "alias": null,
+          "name": "edges",
+          "storageKey": null,
+          "args": null,
+          "concreteType": "LinkEdge",
+          "plural": true,
+          "selections": [
+            {
+              "kind": "LinkedField",
+              "alias": null,
+              "name": "node",
+              "storageKey": null,
+              "args": null,
+              "concreteType": "Link",
+              "plural": false,
+              "selections": [
+                {
+                  "kind": "ScalarField",
+                  "alias": null,
+                  "name": "__typename",
+                  "args": null,
+                  "storageKey": null
+                },
+                {
+                  "kind": "FragmentSpread",
+                  "name": "Link_link",
+                  "args": null
+                }
+              ]
+            },
+            {
+              "kind": "ScalarField",
+              "alias": null,
+              "name": "cursor",
+              "args": null,
+              "storageKey": null
+            }
+          ]
+        },
+        {
+          "kind": "LinkedField",
+          "alias": null,
+          "name": "pageInfo",
+          "storageKey": null,
+          "args": null,
+          "concreteType": "PageInfo",
+          "plural": false,
+          "selections": [
+            {
+              "kind": "ScalarField",
+              "alias": null,
+              "name": "hasPreviousPage",
+              "args": null,
+              "storageKey": null
+            },
+            {
+              "kind": "ScalarField",
+              "alias": null,
+              "name": "startCursor",
+              "args": null,
+              "storageKey": null
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
+// prettier-ignore
+(node/*: any*/).hash = 'c8b60409488c6ebd4c9f08f9c5eb8395';
+
+module.exports = node;
